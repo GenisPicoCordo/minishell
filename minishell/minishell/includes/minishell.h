149a68b8f886e67d7528376e9952f906d4ecb098
Genís Picó Cordo <genis.pico@hotmail.com>
2025-04-01 15:52:30 +0200

    Update minishell.h

--- minishell/minishell/includes/minishell.h
+++ minishell/minishell/includes/minishell.h
@@ -1,6 +1,41 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "../libft/libft.h"
 # include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include <sys/types.h>
+
+//static int			g_var;
+
+typedef enum e_type_tokens
+{
+	T_COMMANDS,
+	T_ARGUMENT
+}	t_token_type;
+
+typedef struct s_token
+{
+	char			*content;
+	t_token_type	type;
+	struct s_token	*next;
+}	t_token;
+
+//MAIN and LOOP functions
+t_token	*new_token(char *content, t_token_type type);
+
+//EXECUTOR
+int		count_tokens(t_token *list);
+char	**build_argv(t_token *tokens);
+void	execute_tokens(t_token *tokens, char **env);
+
+//LIBERATIONS
+void	free_tokens(t_token *tokens);
 
 #endif
